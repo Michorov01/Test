4f5444c5c3bb042e2f63cd570fff9fbec41ea933
Michorov01 <vasil.michorov123@outlook.com>
2019-04-06 18:37:44 +0300

    Edit
    Editing

--- TestVS/TestVS/TestVS.cpp
+++ TestVS/TestVS/TestVS.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "asdasdasdasda"; 
+    cout << "asdasdasdasda"; 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
